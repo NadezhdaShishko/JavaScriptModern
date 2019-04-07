d1cafbc11318ebd93cc35ef3339a6d4d3717074f
NadezhdaShishko <Naumova_nadegda@mail.ru>
2019-04-08 00:56:15 +0300

    в компоненты: поиск, корзина + удаление товара

--- lesson6/public/app.js
+++ lesson6/public/app.js
@@ -10,7 +10,7 @@ Vue.component('product-item', {
                 </div>
                 <img :src="item.image" alt="img"/>
             </div>
-            <h3>{{ item.name }}</h3><span>{{ item.price }}</span>
+            <h3>{{ item.name }}</h3><span>&#36 {{ item.price }}</span>
         </div>
     `,
     methods: {
@@ -18,40 +18,130 @@ Vue.component('product-item', {
             this.$emit('onBuy', item);
         }
     }
-})
+});
 
 Vue.component('products', {
-    props: ['items'],
+    props: ['query'],
     template: `
-        <div class="goodsList" v-if="items.length">
-            <div class="goodsItem" v-for="item in items">
-                <product-item @onBuy="handleBuyClick" :item="item"><product-item>
+        <div>
+            <div class="goodsList" v-if="filteredItems.length">
+                <div class="goodsItem" v-for="item in filteredItems">
+                    <product-item @onBuy="handleBuyClick" :item="item"><product-item>
+                </div>
             </div>
-        </div>
+            <div v-if="!filteredItems.length" class="empty">Ничего не найдено</div>
+        </div>    
     `,
+    data() {
+        return {
+            items: [],
+        }
+    },
     methods: {
         handleBuyClick(item) {
             this.$emit('onbuy', item);
         }
+    },
+    computed: {
+        filteredItems() {
+            const regexp = new RegExp(this.query, 'i');
+            return this.items.filter((item) => regexp.test(item.name))
+        },
+    },
+    mounted() {
+        fetch(`${API_URL}/goods`)
+        .then((response) => response.json())
+        .then((items) => {
+            this.items = items;
+        });
     }
 });
 
 Vue.component('search', {
-    props: ['item', 'searchQuery'],
     template: `
-    <input type="text" v-model="searchQuery" class="searchQuery" placeholder="Поиск по названию товара" />
-    `
-})
+        <div>
+            <input type="text" v-model="searchQuery" class="searchQuery" placeholder="Поиск по названию товара" />
+            <button id="search" @click="hendleSearchClick">Поиск</button>
+        </div>
+    `,
+    data() {
+        return {
+            searchQuery: '',
+        };
+    },
+    methods: {
+        hendleSearchClick() {
+            this.$emit('onsearch', this.searchQuery);
+        }
+    }
+});
+
+Vue.component('cart', {
+    props: ['cart'],
+    template:`
+        <div>
+            <h3 class="total" v-if="cart.length">В корзине товара на сумму:<span> {{ total }} $</span>.</h3>
+            <h3 class="total" v-if="!cart.length">Корзина пуста.</h3>
+            <ul id="cartList" v-if="cart.length">
+                <li v-for="item in cart">
+                <img :src="item.image" alt="img"/>
+                <h3>{{ item.name }}</h3>
+                <span>&#36{{ item.price }}</span>
+                <span>{{ item.quantity }}</span>
+                <span>&#36{{ item.subtotal }}</span>
+                <button class="action" @click="handleDeleteClick(item)"><i class="fas fa-times-circle"></i></button>
+                </li>
+            </ul>
+        </div>
+    `,
+    computed: {
+        total() {
+            return this.cart.reduce((acc, item) => acc + item.quantity * item.price, 0);
+        }
+    },
+    methods: {
+        handleDeleteClick(item) {
+            this.$emit('ondelete', item);
+        }
+    }
+});
 
 const app = new Vue({
     el: '#app',
     data: {
-        items: [],
         cart: [],
-        searchQuery: '',
-        display: 'none'
+        searchQuery: ''
+    },
+    mounted() {
+        fetch(`${API_URL}/cart`)
+            .then((response) => response.json())
+            .then((items) => {
+                this.cart = items;
+            });
     },
     methods: {
+        handleSearch(query) {
+            this.searchQuery = query;
+        },
+        handleDeleteClick(item) {
+            if(item.quantity > 1) {
+                fetch(`${API_URL}/cart/${item.id}`, {
+                    method: 'PATCH',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({quantity: item.quantity - 1})
+                }).then((response) => response.json())
+                  .then((updated) => {
+                    const itemIdx = this.cart.findIndex(cartItem => cartItem.id === item.id);
+                    Vue.set(this.cart, itemIdx, updated);
+                });
+            } else {
+                fetch(`${API_URL}/cart/${item.id}`, {
+                    method: 'DELETE',
+                }).then(()=> {
+                    this.cart = this.cart.filter((cartItem) => cartItem.id !== item.id);
+                });
+            }
+        },
         handleBuyClick(item) {
             const cartItem = this.cart.find(cartItem => cartItem.id === item.id);
             if(cartItem) {
@@ -74,24 +164,6 @@ const app = new Vue({
                     this.cart.push(created);
                 });
             }
-        }
-    },
-    mounted() {
-        fetch(`${API_URL}/goods`)
-        .then((response) => response.json())
-        .then((items) => {
-            this.items = items;
-            this.filteredItems = items;
-            this.display = 'block';
-        });
-    },
-    computed: {
-        filteredItems() {
-            const regexp = new RegExp(this.searchQuery, 'i');
-            return this.items.filter((item) => regexp.test(item.name))
-        },
-        total() {
-            return this.cart.reduce((acc, item) => acc + item.quantity * item.price, 0);
         }
     }
 });
